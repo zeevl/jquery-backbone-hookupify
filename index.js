bce5b1611d30e34f52d221c536979c5e1bda6bbd
Steve Lamb <steve@stevelamb.io>
2014-01-27 16:27:47 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+var through = require('through');
+
+function hookupBbJs(buf) {
+  // find $ = require('jquery'); or similar, in coffee or js
+	var jq = /([\S]+)\s*=\s*require\(*'jquery'\)*(;*)/.exec(buf);
+
+	//add the line Backbone.$ = $ following require('backbone')
+	return jq == null ? buf : 
+		buf.replace(
+			/([\S]+)\s*=\s*require\(*'backbone'\)*;*/, 
+			"$&\n$1.$ = " + jq[1] + jq[2]);
+}
+
+module.exports = function(file) {
+	var data = '';
+	return through(write, buf)
+
+	function write(buf) {
+		data += buf;
+	}
+
+	function end() {
+		this.queue(hookupBbJs(buf));
+		this.queue(null);
+	}
+}
